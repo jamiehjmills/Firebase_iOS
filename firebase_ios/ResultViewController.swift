292a3cef7e7d4e2bff3aafaa0581d9900971082b
Hyojoo Mills <hyojoo.mills@gmail.com>
2022-08-07 21:29:06 +1000

    first commit

---
+++ firebase_ios/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  firebase_ios
+//
+//  Created by Hyojoo Mills on 7/8/2022.
+//
+
+import SwiftUI
+
+struct ResultViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultViewController()
+    }
+}
---
+++ firebase_ios/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  viewController.swift
+//  firebase_ios
+//
+//  Created by Hyojoo Mills on 7/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    
+    @IBAction func click(_ sender: UIButton) {
+        print("test")
+    }
+    
+}
+
